90c963225733db4b60dbeb1e5f380671223aad39
itsmyCareer <57393031+itsmyCareer@users.noreply.github.com>
2019-11-08 13:28:04 +0900

    오류 발생 제거

--- Server/main.py
+++ Server/main.py
@@ -5,19 +5,31 @@ import pymysql.cursors
 db = pymysql.connect(host='localhost', port=3306, user='itsmysurport', passwd='Wordp@ss5479', db='testdb', charset='utf8mb4')
 app = Flask(__name__)
 
-@app.route('/', methods = ['POST', 'GET'])
-def test():
-    a = request.json['username']
-    try:
+try:
+    @app.route('/', methods = ['POST', 'GET'])
+    def test():
+        a = request.json['username']
+        
+        with db.cursor() as cursor:
+            sql = 'SELECT * FROM users'
+            cursor.execute(sql)
+            result = cursor.fetchall()
+
+            for i in range(0, len(result)):
+                if result[i][0] == a:
+                    break
+        
         with db.cursor() as cursor:
             sql = 'INSERT INTO users (username) VALUES (%s)'
             cursor.execute(sql, (a))
         db.commit()
         print(cursor.lastrowid)
         # 1 (last insert id)
-    finally:
-        db.close()
-    return 'success'
+        
+        return 'success'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    if __name__ == '__main__':
+        app.run(debug=True)
+
+finally:
+        db.close()
